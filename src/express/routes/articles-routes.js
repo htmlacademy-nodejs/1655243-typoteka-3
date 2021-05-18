b42a733f87a14171b0799b1ef29a2098c60feb4d
Vladislav <vlad74rus.rus@gmail.com>
2021-05-18 22:07:28 +0500

    chore: adds view templates with resources

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const {Router} = require(`express`);
+const {
+  Router
+} = require(`express`);
+
 const articlesRoutes = new Router();
 
-articlesRoutes.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
-articlesRoutes.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRoutes.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-articlesRoutes.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+articlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
+articlesRoutes.get(`/add`, (req, res) => res.render(`articles/new-post`));
+articlesRoutes.get(`/edit/:id`, (req, res) => res.render(`articles/new-post`));
+articlesRoutes.get(`/:id`, (req, res) => res.render(`articles/post`));
 
 module.exports = articlesRoutes;
