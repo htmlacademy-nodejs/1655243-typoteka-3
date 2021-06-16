1f01aace19a536f6232452fd416570ad4c5edff2
Vladislav <vlad74rus.rus@gmail.com>
2021-06-16 21:53:10 +0500

    chore: adds real data to front

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -1,14 +1,69 @@
 'use strict';
 
-const {
-  Router
-} = require(`express`);
+const {Router} = require(`express`);
+const multer = require(`multer`);
+const path = require(`path`);
+const {nanoid} = require(`nanoid`);
+const api = require(`../api`).getAPI();
 
-const articlesRoutes = new Router();
+const UPLOAD_DIR = `../upload/img`;
+const uploadDirAbsolutePath = path.resolve(__dirname, UPLOAD_DIR);
 
-articlesRoutes.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
-articlesRoutes.get(`/add`, (req, res) => res.render(`articles/new-post`));
-articlesRoutes.get(`/edit/:id`, (req, res) => res.render(`articles/new-post`));
-articlesRoutes.get(`/:id`, (req, res) => res.render(`articles/post`));
+const storage = multer.diskStorage({
+  destination: uploadDirAbsolutePath,
+  filename: (req, file, cb) => {
+    const uniqueName = nanoid(10);
+    const extension = file.originalname.split(`.`).pop();
+    cb(null, `${uniqueName}.${extension}`);
+  },
+});
 
-module.exports = articlesRoutes;
+const articlesRouter = new Router();
+const upload = multer({storage});
+
+articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
+
+articlesRouter.get(`/add`, async (req, res) => {
+  const categories = await api.getCategories();
+  res.render(`articles/new-post`, {categories});
+});
+
+articlesRouter.post(`/add`, upload.single(`upload`), async (req, res) => {
+  try {
+    const {body, file} = req;
+
+    const articleData = {
+      createdDate: body.date,
+      title: body.title,
+      category: body.category || [],
+      announce: body.announcement,
+      fullText: body[`full-text`],
+    };
+
+    if (file) {
+      articleData.picture = file.filename;
+    }
+
+    await api.createArticle(articleData);
+    res.redirect(`/my`);
+  } catch (err) {
+    res.redirect(`back`);
+  }
+});
+
+articlesRouter.get(`/edit/:id`, async (req, res, next) => {
+  try {
+    const {id} = req.params;
+    const [article, categories] = await Promise.all([
+      api.getArticleById(id),
+      api.getCategories(),
+    ]);
+    res.render(`articles/edit-post`, {article, categories});
+  } catch (err) {
+    next();
+  }
+});
+
+articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post`));
+
+module.exports = articlesRouter;
